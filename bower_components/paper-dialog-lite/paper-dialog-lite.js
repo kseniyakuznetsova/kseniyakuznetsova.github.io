7aea65f69d93a41295be5decc39e43f916f6dcd7
Kseniya Kuznetsova <kseniya9696@gmail.com>
2018-05-05 15:09:14 +0400

    first commit

---
+++ bower_components/paper-dialog-lite/paper-dialog-lite.js
@@ -0,0 +1,1 @@
+class PaperDialogLite extends Polymer.mixinBehaviors([Polymer.IronOverlayBehavior],Polymer.Element){static get template(){return`<slot></slot>`}}customElements.define("paper-dialog-lite",PaperDialogLite);
